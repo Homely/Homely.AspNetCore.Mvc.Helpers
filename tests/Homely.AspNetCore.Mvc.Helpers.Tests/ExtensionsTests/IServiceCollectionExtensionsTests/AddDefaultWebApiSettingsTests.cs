7aee7b48964c47a642b855c10178456510c1b07c
Justin Adler <justin@homely.com.au>
2021-11-25 09:50:58 +1100

    More tests

---
+++ tests/Homely.AspNetCore.Mvc.Helpers.Tests/ExtensionsTests/IServiceCollectionExtensionsTests/AddDefaultWebApiSettingsTests.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using Homely.AspNetCore.Mvc.Helpers.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace Homely.AspNetCore.Mvc.Helpers.Tests.ExtensionsTests.IServiceCollectionExtensionsTests
+{
+    public class AddDefaultWebApiSettingsTests
+    {
+        public static TheoryData<string?, IEnumerable<Action<IMvcBuilder>>?> Data =>
+            new TheoryData<string?, IEnumerable<Action<IMvcBuilder>>?>
+            {
+                // Nothing.
+                {
+                    null,
+                    null
+                },
+
+                // Banner but no method chaining.
+                {
+                    "some banner",
+                    null
+                },
+
+                // Empty method chaining.
+                {
+                    "some banner",
+                    new List<Action<IMvcBuilder>>()
+                },
+
+                // Banner + 1 other method.
+                {
+                     "some banner",
+                     new List<Action<IMvcBuilder>>()
+                     {
+                         new Action<IMvcBuilder>(_ => { var i = 1; i++; })
+                     }
+                },
+
+                // Banner + multiple other methods.
+                {
+                     "some banner",
+                     new List<Action<IMvcBuilder>>()
+                     {
+                         new Action<IMvcBuilder>(_ => { var i = 1; i++; }),
+                         new Action<IMvcBuilder>(_ => { var j = 1; j++; }),
+                         new Action<IMvcBuilder>(_ => { var k = 1; k++; })
+                     }
+                }
+            };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void GivenSomeSettings_AddDefaultWebApiSettings_ShouldSetupTheSettings(string? banner,
+            IEnumerable<Action<IMvcBuilder>>? otherMethods)
+        {
+            // Arrange.
+            var services = new ServiceCollection();
+
+            // Act.
+            var sameServices = services.AddDefaultWebApiSettings(banner: banner, otherChainedMethods: otherMethods);
+
+            // Assert.
+            sameServices.ShouldNotBeNull();
+        }
+    }
+}
--- tests/Homely.AspNetCore.Mvc.Helpers.Tests/TestControllerTests/DynamicErrorTests.cs
+++ tests/Homely.AspNetCore.Mvc.Helpers.Tests/TestControllerTests/DynamicErrorTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Shouldly;
 using System;
@@ -35,8 +35,8 @@ namespace Homely.AspNetCore.Mvc.Helpers.Tests.TestControllerTests
             // TODO: Set IsDevelopment() so we can proove that both error messages are different.
 
             // Act.
-            var response1 = await _factory.CreateClient().GetAsync(route);
-            var response2 = await _factory.CreateClient().GetAsync(route);
+            var response1 = await client.GetAsync(route);
+            var response2 = await client.GetAsync(route);
 
             // Assert.
             response1.StatusCode.ShouldBe(HttpStatusCode.InternalServerError);
